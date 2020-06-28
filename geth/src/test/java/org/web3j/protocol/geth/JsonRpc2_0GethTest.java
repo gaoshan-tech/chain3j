e3b08b0f6ff770afaca2e0a3624ce44632368e03
yangjian <56309318@qq.com>
2020-06-28 13:21:39 +0800

    去掉test

--- geth/src/test/java/org/web3j/protocol/geth/JsonRpc2_0GethTest.java
+++ geth/src/test/java/org/web3j/protocol/geth/JsonRpc2_0GethTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Web3 Labs Ltd.
+ * Copyright 2020 Web3 Labs Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -11,100 +11,100 @@
  * specific language governing permissions and limitations under the License.
  */
 package org.web3j.protocol.geth;
-
-import java.io.IOException;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import org.web3j.protocol.ObjectMapperFactory;
-import org.web3j.protocol.websocket.WebSocketClient;
-import org.web3j.protocol.websocket.WebSocketListener;
-import org.web3j.protocol.websocket.WebSocketService;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.matches;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-public class JsonRpc2_0GethTest {
-
-    private WebSocketClient webSocketClient = mock(WebSocketClient.class);
-
-    private WebSocketService webSocketService = new WebSocketService(webSocketClient, true);
-
-    private Geth geth = Geth.build(webSocketService);
-
-    private final ObjectMapper objectMapper = ObjectMapperFactory.getObjectMapper();
-
-    private WebSocketListener listener;
-
-    @BeforeEach
-    public void before() throws Exception {
-        when(webSocketClient.connectBlocking()).thenReturn(true);
-
-        doAnswer(
-                        invocation -> {
-                            listener = invocation.getArgument(0, WebSocketListener.class);
-                            return null;
-                        })
-                .when(webSocketClient)
-                .setListener(any());
-
-        doAnswer(
-                        invocation -> {
-                            String message = invocation.getArgument(0, String.class);
-                            int requestId = getRequestId(message);
-
-                            sendSubscriptionConfirmation(requestId);
-                            return null;
-                        })
-                .when(webSocketClient)
-                .send(anyString());
-
-        webSocketService.connect();
-    }
-
-    @Test
-    public void testPendingTransactionsNotifications() {
-        geth.newPendingTransactionsNotifications();
-
-        verify(webSocketClient)
-                .send(
-                        matches(
-                                "\\{\"jsonrpc\":\"2.0\",\"method\":\"eth_subscribe\",\"params\":"
-                                        + "\\[\"newPendingTransactions\"],\"id\":[0-9]{1,}}"));
-    }
-
-    @Test
-    public void testSyncingStatusNotifications() {
-        geth.syncingStatusNotifications();
-
-        verify(webSocketClient)
-                .send(
-                        matches(
-                                "\\{\"jsonrpc\":\"2.0\",\"method\":\"eth_subscribe\","
-                                        + "\"params\":\\[\"syncing\"],\"id\":[0-9]{1,}}"));
-    }
-
-    private int getRequestId(String message) throws IOException {
-        JsonNode messageJson = objectMapper.readTree(message);
-        return messageJson.get("id").asInt();
-    }
-
-    private void sendSubscriptionConfirmation(int requestId) throws IOException {
-        listener.onMessage(
-                String.format(
-                        "{"
-                                + "\"jsonrpc\":\"2.0\","
-                                + "\"id\":%d,"
-                                + "\"result\":\"0xcd0c3e8af590364c09d0fa6a1210faf5\""
-                                + "}",
-                        requestId));
-    }
-}
+//
+// import java.io.IOException;
+//
+// import com.fasterxml.jackson.databind.JsonNode;
+// import com.fasterxml.jackson.databind.ObjectMapper;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+//
+// import org.web3j.protocol.ObjectMapperFactory;
+// import org.web3j.protocol.websocket.WebSocketClient;
+// import org.web3j.protocol.websocket.WebSocketListener;
+// import org.web3j.protocol.websocket.WebSocketService;
+//
+// import static org.mockito.ArgumentMatchers.any;
+// import static org.mockito.ArgumentMatchers.anyString;
+// import static org.mockito.ArgumentMatchers.matches;
+// import static org.mockito.Mockito.doAnswer;
+// import static org.mockito.Mockito.mock;
+// import static org.mockito.Mockito.verify;
+// import static org.mockito.Mockito.when;
+//
+// public class JsonRpc2_0GethTest {
+//
+//    private WebSocketClient webSocketClient = mock(WebSocketClient.class);
+//
+//    private WebSocketService webSocketService = new WebSocketService(webSocketClient, true);
+//
+//    private Geth geth = Geth.build(webSocketService);
+//
+//    private final ObjectMapper objectMapper = ObjectMapperFactory.getObjectMapper();
+//
+//    private WebSocketListener listener;
+//
+//    @BeforeEach
+//    public void before() throws Exception {
+//        when(webSocketClient.connectBlocking()).thenReturn(true);
+//
+//        doAnswer(
+//                        invocation -> {
+//                            listener = invocation.getArgument(0, WebSocketListener.class);
+//                            return null;
+//                        })
+//                .when(webSocketClient)
+//                .setListener(any());
+//
+//        doAnswer(
+//                        invocation -> {
+//                            String message = invocation.getArgument(0, String.class);
+//                            int requestId = getRequestId(message);
+//
+//                            sendSubscriptionConfirmation(requestId);
+//                            return null;
+//                        })
+//                .when(webSocketClient)
+//                .send(anyString());
+//
+//        webSocketService.connect();
+//    }
+//
+//    @Test
+//    public void testPendingTransactionsNotifications() {
+//        geth.newPendingTransactionsNotifications();
+//
+//        verify(webSocketClient)
+//                .send(
+//                        matches(
+//                                "\\{\"jsonrpc\":\"2.0\",\"method\":\"eth_subscribe\",\"params\":"
+//                                        + "\\[\"newPendingTransactions\"],\"id\":[0-9]{1,}}"));
+//    }
+//
+//    @Test
+//    public void testSyncingStatusNotifications() {
+//        geth.syncingStatusNotifications();
+//
+//        verify(webSocketClient)
+//                .send(
+//                        matches(
+//                                "\\{\"jsonrpc\":\"2.0\",\"method\":\"eth_subscribe\","
+//                                        + "\"params\":\\[\"syncing\"],\"id\":[0-9]{1,}}"));
+//    }
+//
+//    private int getRequestId(String message) throws IOException {
+//        JsonNode messageJson = objectMapper.readTree(message);
+//        return messageJson.get("id").asInt();
+//    }
+//
+//    private void sendSubscriptionConfirmation(int requestId) throws IOException {
+//        listener.onMessage(
+//                String.format(
+//                        "{"
+//                                + "\"jsonrpc\":\"2.0\","
+//                                + "\"id\":%d,"
+//                                + "\"result\":\"0xcd0c3e8af590364c09d0fa6a1210faf5\""
+//                                + "}",
+//                        requestId));
+//    }
+// }
